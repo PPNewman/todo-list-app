4f00939e95b14e331a45c297de3ceb038d61d657
Przemyslaw Nowak <nowakpprzemyslaw@gmail.com>
2020-06-26 19:20:38 +0200

    1. Implement complete task button 2. Show and cancel edit task popup

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ let $alertInfo; //info o braku zadań / konieczności dodania zadania
 let $btnAdd; // btn dodający zadanie z inputu do listy
 let $ulList; // lista zadań / tag <ul></ul>
 let $newTask; // nowo utworzone zadanie w li
-
+let $popup; //pobrany popup
+let $popupInfo; //alert w popupie jak się doda pusty tekst
+let $editetTodo; //edytowany Todo
+let $popupInput; // teskst wpisny w input popupa
+let $btnAddPopup; // btn "zatwierdź" w popupie
+let $btnCloseTodo; //btn do zamykania popupa
 
 const main = () => {
     prepareDOMelements();
@@ -15,6 +20,11 @@ const prepareDOMelements = () => {
     $alertInfo = document.querySelector('.alertInfo');
     $btnAdd = document.querySelector('.addBtn');
     $ulList = document.querySelector('.todoList ul');
+    $popup = document.querySelector('.popup');
+    $popupInfo = document.querySelector('.popupInfo');
+    $popupInput = document.querySelector('.popupInput');
+    $btnAddPopup = document.querySelector('.accept');
+    $btnCloseTodo = document.querySelector('.cancel');
 };
 
 const addNewTask = () => {
@@ -56,9 +66,28 @@ const creatToolsArea = () => {
 // prepareDOMevents nadaje nasłuchiwanie
 const prepareDOMevents = () => {
     $btnAdd.addEventListener('click', addNewTask);
-
+    $ulList.addEventListener('click', checkClick);
+    $btnCloseTodo.addEventListener('click', closePopup);
 };
 
+const checkClick = (event) => {
+    if (event.target.closest('button').classList.contains('complete')) {
+        event.target.closest('li').classList.toggle('completed');
+        event.target.closest('button').classList.toggle('completed');
+    } else if (event.target.closest('button').className === 'edit') {
+        editTask();
+    } else if (event.target.closest('button').className === 'delete') {
+
+    }
+};
+
+const editTask = () => {
+    $popup.style.display = 'flex';
+};
+
+const closePopup = () => {
+    $popup.style.display = 'none';
+};
 
 document.addEventListener('DOMContentLoaded', main);
 // Nasłuch gdy załaduje się całe drzewo DOM (strona)to wtedy ma odpalić funkcję main
